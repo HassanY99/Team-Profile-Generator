8387bf05ea8e110ec4fd66add3af55d16bb1a15c
Hassan Yusuf <hassanyousuf1999@gmail.com>
2021-08-10 19:56:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./assets/lib/Employee');
+const Engineer = require('./assets/lib/Engineer');
+const Intern = require('./assets/lib/Intern');
+const Manager = require('./assets/lib/Manager');
+
+const employs = [];
